218a746b5e0a7d3f3305f5bf568caa0fd7660ddb
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-10-25 08:27:06 +0200

    change the jsx elements structure in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+          <h1>Do zrobienia</h1>
+          <h2>5 zadań</h2>
+          <button>+</button>
+      </div>
+  )
+}
+
+export default App
