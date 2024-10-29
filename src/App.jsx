1d6a95c0da37f7c8e0ef2ca6d561e5446052f7dc
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-10-29 07:52:49 +0100

    add Form component to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import styles from './App.module.css'
+import {Form} from "./components/Form.jsx";
 
 function App() {
-  return (
-      <div>
-          <h1>Do zrobienia</h1>
-          <h2>5 zadań</h2>
-          <button>+</button>
-      </div>
-  )
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1>Do zrobienia</h1>
+                <h2>5 zadań</h2>
+                <button className={styles.button}>+</button>
+            </header>
+            <Form/>
+        </div>
+
+    )
 }
 
 export default App
