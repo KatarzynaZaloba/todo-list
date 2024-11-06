3cf686de2e5f85ce81ca273f947f01f383b3126d
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-11-06 08:41:20 +0100

    import helper function getSubheading.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import styles from './App.module.css'
 import {Form} from "./components/Form/Form.jsx";
 import {TodoItem} from "./components/TodoItem/TodoItem";
+import {getSubheading} from "./utils/getSubheading.js";
 
 function App() {
     const todos = [
         {name: "Zapłacić rachunki", done: false, id: 1},
         {name: "Wyrzucić śmieci", done: true, id: 2},
     ];
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 <h1>Do zrobienia</h1>
-                <h2>5 zadań</h2>
+                <h2>{getSubheading(todos.length)}</h2>
                 <button className={styles.button}>+</button>
             </header>
             <Form/>
