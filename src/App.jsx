b472a94ce3ace015ecbaca32c8ef476897255d4e
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-11-07 08:30:40 +0100

    add setIsFormShown and setTodos setters, add logic for rendering button and Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,34 @@ import styles from './App.module.css'
 import {Form} from "./components/Form/Form.jsx";
 import {TodoItem} from "./components/TodoItem/TodoItem";
 import {getSubheading} from "./utils/getSubheading.js";
+import {useState} from "react";
 
 function App() {
-    const todos = [
+    const [isFormShown, setIsFormShown] = useState(false);
+    const [todos, setTodos] = useState([
         {name: "Zapłacić rachunki", done: false, id: 1},
         {name: "Wyrzucić śmieci", done: true, id: 2},
-    ];
+    ]);
 
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 <h1>Do zrobienia</h1>
                 <h2>{getSubheading(todos.length)}</h2>
-                <button className={styles.button}>+</button>
+                {!isFormShown && (
+                    <button onClick={() => setIsFormShown(true)}
+                            className={styles.button}
+                    >+
+                    </button>
+                )}
             </header>
-            <Form/>
+            {isFormShown && (
+                <Form
+                    onFormSubmit={(newTodoName) => {
+                        alert(newTodoName);
+                    }}
+                />
+            )}
             <ul>
                 {todos.map(({id, name, done}) => (
                     <TodoItem
