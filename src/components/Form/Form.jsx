3b02aba4c387e4313150e10c874eef6e51a7d73e
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-11-07 08:31:44 +0100

    add setInputValue setter; add onSubmit event in form; add onChange event and value for input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,30 @@
 import styles from './Form.module.css';
 import {Button} from "../Button/Button.jsx";
+import {useState} from "react";
 
-export function Form() {
+export function Form({onFormSubmit}) {
+    const [inputValue, setInputValue] = useState("");
+
     return (
-        <form className={styles.form} action="">
-            <input className={styles.input} type="text"/>
-            <Button onClick={() => {alert('test')}}>Dodaj</Button>
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                onFormSubmit(inputValue);
+            }}
+            className={styles.form} action=""
+        >
+            <input
+                onChange={(event) => {
+                    setInputValue(event.target.value)
+                }}
+                value={inputValue}
+                className={styles.input}
+                type="text"
+            />
+            <Button
+            >
+                Dodaj
+            </Button>
         </form>
     );
 }
