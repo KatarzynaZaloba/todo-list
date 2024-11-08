64116020661416318f49391549e34145e0dd233d
KatarzynaAfrontech <kzalobafrontend@gmail.com>
2024-11-08 08:46:59 +0100

    add deleteItem, finishItem and addItem functions

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,31 @@ function App() {
         {name: "Wyrzucić śmieci", done: true, id: 2},
     ]);
 
+    function addItem(newTodoName) {
+        setTodos((prevTodos) => [...prevTodos, {
+            name: newTodoName,
+            done: false,
+            id: prevTodos.length > 0 ? prevTodos.at(-1).id + 1 : 0,
+        }]);
+        setIsFormShown(false);
+    }
+
+    function deleteItem(id) {
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    }
+
+    function finishItem(id) {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            if (todo.id !== id) {
+                return todo;
+            }
+            return {
+                ...todo,
+                done: true,
+            }
+        }))
+    }
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
@@ -25,9 +50,7 @@ function App() {
             </header>
             {isFormShown && (
                 <Form
-                    onFormSubmit={(newTodoName) => {
-                        alert(newTodoName);
-                    }}
+                    onFormSubmit={(newTodoName) => addItem(newTodoName)}
                 />
             )}
             <ul>
@@ -36,6 +59,8 @@ function App() {
                         key={id}
                         name={name}
                         done={done}
+                        onDeleteButtonClick={() => deleteItem(id)}
+                        onDoneButtonClick={() => finishItem(id)}
                     />
                 ))}
             </ul>
